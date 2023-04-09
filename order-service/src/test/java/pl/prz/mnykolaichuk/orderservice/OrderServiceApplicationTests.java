1515db352f645e6c73882749dadf7101dd2ce890
Mykola Nykolaichuk <52537767+mnykolaichuk96@users.noreply.github.com>
2023-04-09 12:09:13 +0200

    Created order service Created Controller with post and get methods for order

---
+++ order-service/src/test/java/pl/prz/mnykolaichuk/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.prz.mnykolaichuk.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
